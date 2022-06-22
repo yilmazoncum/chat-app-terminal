d0eb869ca35df7a2eef1795189d60956e223f51e
yilmazoncum <oncumkorkmaz@gmail.com>
2022-06-22 10:06:18 +0300

    add js and html files

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import {Server} from 'socket.io'
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd()+ '/index.html');
+  });
+  
+io.on('connection', (socket) => {
+socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+
+  });
+ 
+  
+  server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
