ff13894d1144e7f075ff3708a4d1d8701a1c399f
yilmazoncum <oncumkorkmaz@gmail.com>
2022-06-22 12:09:06 +0300

    change for terminal chatting

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+
+  const port = 3000
+
+  io.on('connection', (socket) => {
+      console.log('connected')
+      socket.on('message', (evt) => {
+        console.log(evt)
+        socket.broadcast.emit('message', evt)
+    })
+  })
+
+  io.on('disconnect', (evt) => {
+      console.log('disconnected')
+  })
+
+  http.listen(port, () => console.log(`server listening on port: ${port}`))
